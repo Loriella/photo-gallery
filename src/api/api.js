82f360f0656aea6449cc6f537108bd0864730c22
Loriella <loris.orlova@gmail.com>
2020-04-24 18:42:17 +0300

    add api

---
+++ src/api/api.js
@@ -0,0 +1,32 @@
+export const API_URL = "https://jsonplaceholder.typicode.com";
+
+const fetchApi = (url) => {
+  return fetch(`${API_URL}${url}`)
+    .then(response => {
+      if (response.status < 400) {
+        return response.json();
+      } else {
+        throw response;
+      }
+    })
+};
+
+export default class CallApi {
+  static getUsers() {
+    return fetchApi(
+      `/users`
+    )
+  };
+
+  static getAlbumsByUserId(userId) {
+    return fetchApi(
+      `/users/${userId}/albums`
+    )
+  };
+
+  static getPhotosByAlbumId(AlbumId) {
+    return fetchApi(
+      `/albums/${AlbumId}/photos`
+    )
+  };
+};
