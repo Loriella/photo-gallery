694c68bfb642d3d6a628781d6ccc7ad408ef3706
Loriella <loris.orlova@gmail.com>
2020-04-24 18:47:06 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import UsersList from "./components/Users/UsersList";
+import Albums from "./components/Albums/Albums";
+import {BrowserRouter} from "react-router-dom";
+import {Route} from "react-router";
+import Photos from "./components/Photos/Photos";
 
-function App() {
-  return (
-    <div className="App"/>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Header/>
+      <div className="container">
+        <Route exact path="/" component={UsersList}/>
+        <Route exact path="/users/:userId/albums" component={Albums}/>
+        <Route path="/users/:userId/albums/:albumId" component={Photos}/>
+      </div>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
