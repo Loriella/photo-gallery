3cdf2467fcbb8ec36672ecee7b8f2a8e496c4674
Loriella <loris.orlova@gmail.com>
2020-04-25 17:41:40 +0300

    Updates

---
+++ precache-manifest.01fd32fc2b4e0dca7570c86b717ea005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be721b5a8c096eeda8a866c481c7b7b5",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "dd3ab95be73bfcdfca96",
+    "url": "/photo-gallery/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "32166955e6a103527665",
+    "url": "/photo-gallery/static/css/main.e5b76748.chunk.css"
+  },
+  {
+    "revision": "dd3ab95be73bfcdfca96",
+    "url": "/photo-gallery/static/js/2.75fba505.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-gallery/static/js/2.75fba505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32166955e6a103527665",
+    "url": "/photo-gallery/static/js/main.8f81c4ab.chunk.js"
+  },
+  {
+    "revision": "411bfa730cf451f1277b",
+    "url": "/photo-gallery/static/js/runtime-main.2f2f54d0.js"
+  }
+]);
